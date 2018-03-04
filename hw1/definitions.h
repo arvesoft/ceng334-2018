11fda189b81ba36d283848203f1e0764a783284e
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2018-03-04 17:53:26 +0300

    Initial server, doesn't contain game loop.

--- hw1/definitions.h
+++ hw1/definitions.h
@@ -1,60 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#ifndef __DEFINITIONS_H__
+#define __DEFINITIONS_H__
+#define CLIENT_FD 0
+#define SERVER_FD 1
+
+#include <math.h>
 
 typedef struct coordinate {
-	int x;
-	int y;
+  int x;
+  int y;
 } coordinate;
 
 typedef struct server_message {
-	coordinate pos;
-	coordinate adv_pos;
-	int object_count;
-	coordinate object_pos[4];
+  coordinate pos;
+  coordinate adv_pos;
+  int object_count;
+  coordinate object_pos[4];
 } server_message;
 
 typedef struct ph_message {
-	coordinate move_request;
+  coordinate move_request;
 } ph_message;
 
 coordinate addCoordinate(coordinate a, coordinate b) {
-	coordinate c;
-	c.x = a.x + b.x;
-	c.y = a.y + b.y;
+  coordinate c;
+  c.x = a.x + b.x;
+  c.y = a.y + b.y;
+  return c;
 }
 
 int mhDistance(coordinate a, coordinate b) {
-	return abs(a.x - b.x) + abs(a.y - b.y);
+  return abs(a.x - b.x) + abs(a.y - b.y);
 }
 
-// to write in the process IDs of hunters and preys
-pid_t preyPIDS;
-pid_t hunterPIDS;
-pid_t wantedList;
-int* obstacleList;
-
-int preyPIDIndex;
-int hunterPIDIndex;
-
-void initializeLists()
-{
-	//~ // clear the lists to all -1
-	//~ for (int i=0; i< numberOfPreys; i++)
-	//~ {
-		//~ preyPIDS[i] = -1;
-	//~ }
-	//~ for (int i=0; i < numberOfHunters; i++)
-	//~ {
-		//~ hunterPIDS[i] = -1;
-	//~ }
-	//~ for (int i=0; i < totalNumber; i++)
-	//~ {
-		//~ wantedList[i] = -1;
-	//~ }
+int InRange(coordinate c, int width, int height) {
+  return c.x >= 0 && c.y >= 0 && c.x < width && c.y < height;
 }
+
+coordinate Move(coordinate cur, int direction) {
+  const int dx[] = {1, -1, 0, 0};
+  const int dy[] = {0, 0, 1, -1};
+  cur.x += dx[direction];
+  cur.y += dy[direction];
+  return cur;
+}
+
+enum ClientType {
+  HUNTER = 0,
+  PREY = 1,
+};
+
+typedef struct {
+  coordinate pos;
+  int energy;
+  enum ClientType client_type;
+  pid_t pid;
+  int pipe_fd[2];
+} Client;
+
+#endif
---
+++ hw1/server.c
@@ -0,0 +1,164 @@
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "definitions.h"
+#define PIPE(fd) socketpair(AF_UNIX, SOCK_STREAM, PF_UNIX, fd)
+
+int map_width;
+int map_height;
+int number_of_obstacles;
+coordinate* obstacles;
+int number_of_hunters;
+Client* hunters;
+int number_of_preys;
+Client* preys;
+char** map;
+
+void ReadInput() {
+  scanf("%d%d", &map_width, &map_height);
+  scanf("%d", &number_of_obstacles);
+  map = (char**)malloc(sizeof(char*) * map_height);
+  for (int i = 0; i < map_height; i++) {
+    map[i] = (char*)malloc(sizeof(char) * map_width);
+    memset(map[i], ' ', map_width);
+  }
+  obstacles = (coordinate*)malloc(sizeof(coordinate) * number_of_obstacles);
+  for (int i = 0; i < number_of_obstacles; i++) {
+    scanf("%d%d", &obstacles[i].x, &obstacles[i].y);
+    map[obstacles[i].x][obstacles[i].y] = 'X';
+  }
+  scanf("%d", &number_of_hunters);
+  hunters = (Client*)malloc(sizeof(Client) * number_of_hunters);
+  for (int i = 0; i < number_of_hunters; i++) {
+    scanf("%d%d%d", &hunters[i].pos.x, &hunters[i].pos.y, &hunters[i].energy);
+    hunters[i].client_type = HUNTER;
+    map[hunters[i].pos.x][hunters[i].pos.y] = 'H';
+  }
+  scanf("%d", &number_of_preys);
+  preys = (Client*)malloc(sizeof(Client) * number_of_preys);
+  for (int i = 0; i < number_of_preys; i++) {
+    scanf("%d%d%d", &preys[i].pos.x, &preys[i].pos.y, &preys[i].energy);
+    preys[i].client_type = PREY;
+    map[preys[i].pos.x][preys[i].pos.y] = 'P';
+  }
+}
+
+void Finish() {
+  free(preys);
+  free(hunters);
+  free(obstacles);
+}
+
+void PrintFooter() {
+  putchar('+');
+  for (int i = 0; i < map_width; i++) putchar('-');
+  putchar('+');
+  putchar('\n');
+}
+
+void PrintMap() {
+  PrintFooter();
+  for (int i = 0; i < map_height; i++) {
+    putchar('|');
+    for (int j = 0; j < map_width; j++) putchar(map[i][j]);
+    putchar('|');
+    putchar('\n');
+  }
+  PrintFooter();
+}
+
+void FillNeighbours(server_message* sm, const char type) {
+  sm->object_count = 0;
+  for (int i = 0; i < 4; i++) {
+    const coordinate neighbour = Move(sm->pos, i);
+    if (InRange(neighbour, map_width, map_height)) {
+      const char elem = map[neighbour.x][neighbour.y];
+      if (elem == type || elem == 'X') {
+        sm->object_pos[sm->object_count++] = neighbour;
+      }
+    }
+  }
+}
+
+void InformPrey(int idx) {
+  Client* const prey = preys + idx;
+  server_message sm;
+  sm.pos = prey->pos;
+  sm.adv_pos = hunters[0].pos;
+  int min_distance = mhDistance(sm.pos, sm.adv_pos);
+  for (int i = 1; i < number_of_hunters; i++) {
+    const int distance = mhDistance(sm.pos, hunters[i].pos);
+    if (min_distance > distance) {
+      min_distance = distance;
+      sm.adv_pos = hunters[i].pos;
+    }
+  }
+  FillNeighbours(&sm, 'P');
+
+  write(prey->pipe_fd[SERVER_FD], &sm, sizeof(server_message));
+}
+
+void InformHunter(int idx) {
+  Client* hunter = preys + idx;
+  server_message sm;
+  sm.pos = hunter->pos;
+  sm.adv_pos = preys[0].pos;
+  int min_distance = mhDistance(sm.pos, sm.adv_pos);
+  for (int i = 1; i < number_of_preys; i++) {
+    const int distance = mhDistance(sm.pos, preys[i].pos);
+    if (min_distance > distance) {
+      min_distance = distance;
+      sm.adv_pos = preys[i].pos;
+    }
+  }
+  FillNeighbours(&sm, 'H');
+
+  write(hunter->pipe_fd[SERVER_FD], &sm, sizeof(server_message));
+}
+
+void InitializeClients() {
+  char width[15];
+  char height[15];
+  sprintf(width, "%d", map_width);
+  sprintf(height, "%d", map_height);
+  char* const argv[] = {width, height, NULL};
+  for (int i = 0; i < number_of_preys; i++) {
+    PIPE(preys[i].pipe_fd);
+    preys[i].pid = fork();
+    if (!preys[i].pid) {
+      dup2(preys[i].pipe_fd[CLIENT_FD], STDIN_FILENO);
+      dup2(preys[i].pipe_fd[CLIENT_FD], STDOUT_FILENO);
+      close(preys[i].pipe_fd[SERVER_FD]);
+      execv("./prey", argv);
+    }
+    close(preys[i].pipe_fd[CLIENT_FD]);
+    InformPrey(i);
+  }
+  for (int i = 0; i < number_of_hunters; i++) {
+    PIPE(hunters[i].pipe_fd);
+    hunters[i].pid = fork();
+    if (!hunters[i].pid) {
+      // client will use fd[0] for read and write.
+      dup2(hunters[i].pipe_fd[0], STDIN_FILENO);
+      dup2(hunters[i].pipe_fd[0], STDOUT_FILENO);
+      close(hunters[i].pipe_fd[1]);
+      execv("./hunter", argv);
+    }
+    close(hunters[i].pipe_fd[0]);
+    InformHunter(i);
+  }
+}
+
+int main() {
+  ReadInput();
+  PrintMap();
+  InitializeClients();
+  Finish();
+  return 0;
+}
