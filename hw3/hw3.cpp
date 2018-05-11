b3400b271a94e4ff1a93f3262849972801f5a4b7
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2018-05-11 15:16:16 +0300

    Tatatatatata

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -337,7 +337,7 @@ bool isInTheVector(const std::vector<unsigned int>& vector,
   return false;
 }
 
-void putInodeInto(struct ext2_inode& inode,
+void putInodeInto(struct ext2_dir_entry* entry_to_put,
                   struct ext2_inode& lost_found_inode) {
   /// printf("BBB: %d %d %d\n", lost_found_inode.i_size,
   /// lost_found_inode.i_mode, lost_found_inode.i_blocks);
@@ -345,41 +345,30 @@ void putInodeInto(struct ext2_inode& inode,
   lseek(fd, BLOCK_OFFSET(lost_found_inode.i_block[0]), SEEK_SET);
   read(fd, block, block_size);
 
-  std::vector<struct ext2_dir_entry> directoryEntries;
-
   unsigned int size = 0;
   unsigned int previousSize = 0;
+  const size_t needed_size = realDirEntrySize(entry_to_put);
   struct ext2_dir_entry* entry = (struct ext2_dir_entry*)block;
-  struct ext2_dir_entry* previousEntry = (struct ext2_dir_entry*)block;
-  // for (int i = 0; i < 100; i++) printf("%d %c\n", block[i], block[i]);
   while (size < lost_found_inode.i_size && entry->inode) {
-    char file_name[EXT2_NAME_LEN + 1];
-    std::memcpy(file_name, entry->name, entry->name_len);
-    file_name[entry->name_len] = 0; /* append null char to the file name */
-    printf("    getChildren: %10u %10u %s\n", entry->inode, entry->rec_len,
-           file_name);
+    printf("    putInodeInto: %10u %10u %.*s\n", entry->inode, entry->rec_len,
+           entry->name_len, entry->name);
 
-    ext2_dir_entry dirEntry = *entry;
-    directoryEntries.push_back(dirEntry);
-
-    previousSize = size;
-    size += entry->rec_len;
     const size_t real_len = realDirEntrySize(entry);
-    previousEntry = entry;
-    entry =
-        (ext2_dir_entry*)((void*)entry + real_len); /* move to the next entry */
+    if (entry->rec_len - real_len >= needed_size) {
+      struct ext2_dir_entry* new_entry =
+          (struct ext2_dir_entry*)((void*)entry + real_len);
+      memcpy(new_entry, entry_to_put, needed_size);
+      new_entry->rec_len = entry->rec_len - real_len;
+      entry->rec_len = real_len;
+      break;
+    }
+    size += entry->rec_len;
+    entry = (ext2_dir_entry*)((void*)entry +
+                              entry->rec_len); /* move to the next entry */
   }
-  // you know the previous size, so only calculate the last entry size and add
-  // to previousSize struct ext2_dir_entry {
-  //       unsigned int   inode;     /* Inode number */
-  //       unsigned short rec_len;   /* Directory entry length */
-  //       unsigned char  name_len;  /* Name length */
-  //       unsigned char  file_type;
-  //       char           name[];    /* File name, up to EXT2_NAME_LEN */
-  // };
-  unsigned int lastEntrySize = realDirEntrySize(entry);
-  unsigned int finalSize = previousSize + lastEntrySize;
-  std::cout << "LAST: " << finalSize << std::endl;
+
+  lseek(fd, BLOCK_OFFSET(lost_found_inode.i_block[0]), SEEK_SET);
+  write(fd, block, block_size);
 }
 
 int main(void) {
@@ -460,7 +449,7 @@ int main(void) {
     struct ext2_inode inode = getInodeInfo(dir_entry->inode);
     std::vector<unsigned int> blocks = GetBlocks(inode);
     activateInode(inode, dir_entry->inode);
-    putInodeInto(inode, lost_found_inode);
+    putInodeInto(dir_entry, lost_found_inode);
     markBlocksAsUsed(blocks);
   }
 
